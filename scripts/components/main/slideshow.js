b9eefebdf4d1ee0ef6319b13bb6b260c4640acc7
Mahima de Silva <mahimaindhot@gmail.com>
2023-02-22 14:32:40 +0530

    slideshow init

---
+++ scripts/components/main/slideshow.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  let current_slide = 1;
+  const slide1 = document.querySelector('.slide[data-slide-id="1"]');
+  const slide2 = document.querySelector('.slide[data-slide-id="2"]');
+  const slide3 = document.querySelector('.slide[data-slide-id="3"]');
+
+
+  const nextSlide = () => {
+    switch (current_slide) {
+      case 1:
+        slide1.classList.add("hide");
+        slide2.classList.remove("hide");
+        current_slide = 2;
+        break;
+      case 2:
+        slide2.classList.add("hide");
+        slide3.classList.remove("hide");
+        current_slide = 3;
+        break;
+      case 3:
+        slide3.classList.add("hide");
+        slide1.classList.remove("hide");
+        current_slide = 1;
+        break;
+    }
+  };
+
+  setInterval(nextSlide, 2000);
+});
